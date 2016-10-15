a3a38a01fdd86ac0863bc73591f5254f0e614a20
Vit Ganchuk <vit.ganchuk@gmail.com>
2016-10-15 14:49:53 -0700

    Merged Async GUI

---
+++ Lines/AsyncGUI/AsyncGUI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncGUI {
+    public partial class Form1 : Form {
+
+        private MyLabel label;
+
+        public Form1() {
+            InitializeComponent();
+            label = new MyLabel();
+            label.Text = "Huraaa!";
+            label.Location = new Point(10, 50);
+            panel1.Controls.Add(label);
+
+        }
+
+        private async void Form1_Click(object sender, EventArgs e) {
+            for (int i = 0; i < 100; i++) {
+                await label.MoveForward();
+            }
+        }
+
+        private async void panel1_Click(object sender, EventArgs e) {
+            for (int i = 0; i < 100; i++) {
+                await label.MoveForward();
+            }
+        }
+    }
+}
---
+++ Lines/AsyncGUI/AsyncGUI/MyLabel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncGUI {
+    class MyLabel : Label {
+
+        public Task MoveForward() {
+            return Task.Run(async () =>
+            {
+
+                if (InvokeRequired) {
+                    Invoke((Action)(() =>
+                    {
+                        Location = new Point(Location.X + 5, Location.Y);
+
+                    }));
+                } else {
+
+                }
+
+                await Task.Delay(1000);
+
+            });
+        }
+    }
+}
